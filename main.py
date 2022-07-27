5ebb1ea2d6f78ce795e385ac69a22d8c0e0a96ca
Saul Hdz Lara <knighthdz97@gmail.com>
2022-07-26 21:06:23 -0500

    🔨 Added app instructions and keyboard detections

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ class DrawInformation:
     GREEN = 0, 255, 0
     RED = 255, 0, 0
     GREY = 128, 128, 128
-    BACKGROUND = WHITE
+    BACKGROUND_COLOR = WHITE
 
+    GRADIENTS = [GREY,(160,160,160),(192,192,192)]
+
+    FONT = pygame.font.SysFont("comicsans", 20)
+    LARGE_FONT = pygame.font.SysFont("comicsans", 30)
     SIDE_PAD = 100
     TOP_PAD = 150
 
@@ -44,6 +48,27 @@ def generate_starting_list(n, min_val, max_val):
 
     return lst
 
+def draw(draw_info):
+    draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+
+    controls = draw_info.FONT.render("R - Reset | SPACE - Start sorting | A - Ascending | D - Descending", 1, draw_info.BLACK)
+    draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2, 5))
+
+
+    sorting = draw_info.FONT.render("I - Insertion Sort | B - Bubble Sort", 1, draw_info.BLACK)
+    draw_info.window.blit(sorting, (draw_info.width/2 - sorting.get_width()/2, 35))
+
+    draw_list(draw_info)
+    pygame.display.update()
+
+def draw_list(draw_info):
+    lst = draw_info.lst
+
+    for i, val in enumerate(lst):
+        x = draw_info.start_x + i * draw_info.block_width
+        y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+        color = draw_info.GRADIENTS[i % 3]
+        pygame.draw.rect(draw_info.window, color, (x,y, draw_info.block_width, draw_info.height))
 
 def main():
     run = True
@@ -55,13 +80,33 @@ def main():
 
     lst = generate_starting_list(n, min_val, max_val)
     draw_info = DrawInformation(800, 600, lst)
+    sorting = False
+    ascending = True
 
     while run:
         clock.tick(60)
+        draw(draw_info)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+            if event.type != pygame.KEYDOWN:
+                continue
+
+            if event.key == pygame.K_r:
+                lst = generate_starting_list(n, min_val, max_val)
+                draw_info.set_list(lst)
+                sorting = False
+
+            elif event.key == pygame.K_SPACE and sorting == False:
+                sorting = True
+
+            elif event.key == pygame.K_a and not sorting:
+                ascending = True
+
+            elif event.key == pygame.K_d and not sorting:
+                ascending = False
 
     pygame.quit()
 
