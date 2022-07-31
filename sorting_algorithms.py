b93a824f12ddf9af146848ac812e668fd2cd1276
Saul Hdz Lara <knighthdz97@gmail.com>
2022-07-30 22:17:23 -0500

    📖 DOC: Add documentation of the class

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,5 +1,27 @@
 class SortingAlgorithms:
+    """This class contains the different sorting algorithms
+
+    @author Saúl Hdz Lara
+    @version 1.0
+    """
+
     def bubble_sort(draw_info, draw_list, ascending=True):
+        """Implementation of the bubble sort algorithm
+
+        Parameters
+        ----------
+        draw_info : DrawInformation class
+            An instance of the class with information to draw
+        draw_list : function
+            A function used to draw in the window
+        ascending : bool, optional
+            A flag used to know how to order the numbers, by default True
+
+        Yields
+        ------
+        bool
+            A flag used to know if the sorting process is finished
+        """
         elements = draw_info.lst
 
         for i in range(len(elements) - 1):
@@ -11,4 +33,3 @@ class SortingAlgorithms:
                     elements[j], elements[j + 1] = elements[j + 1], elements[j]
                     draw_list(draw_info, {j: draw_info.GREEN, j + 1: draw_info.RED}, True)
                     yield True
-        return elements
